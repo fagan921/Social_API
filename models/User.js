0182b72a340810d8659dd26511821b2bf3d59f3a
fagan921 <fagan921@gmail.com>
2025-02-19 18:59:58 -0800

    Initial push/file structure/utils

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match a valid email address']
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual property to get the number of friends
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// Create the User model
+const User = model('User', userSchema);
+
+module.exports = User;
